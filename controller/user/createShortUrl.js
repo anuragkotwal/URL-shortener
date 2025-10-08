5cbd2c87bb672b78ee81bede1dc88bc8a6cd04dc
Anurag Kotwal <anurag@getzuma.com>
2025-10-08 17:04:13 +0530

    fix: add check for existing URL before creating a new short URL

--- controller/user/createShortUrl.js
+++ controller/user/createShortUrl.js
@@ -4,6 +4,22 @@ const catchAsync = require("../../utils/catchAsync");
 
 const createShortUrl = catchAsync(async (req, res, next) => {
   const { originalUrl } = req.body;
+  
+  // Check if the original URL already exists
+  const existingUrl = await urlServices.getShortUrl({ originalUrl });
+  
+  if (existingUrl && existingUrl.err) {
+    const { err } = existingUrl;
+    return next(err);
+  }
+  
+  if (existingUrl) {
+    return res
+      .status(200)
+      .json({ success: true, message: "URL already exists", data: existingUrl });
+  }
+  
+  // Create new short URL if it doesn't exist
   const urlId = new ShortUniqueId({ length: 4 })();
   const shortUrl = `${process.env.BASE_URL}/${urlId}`;
   const body = { urlId, originalUrl, shortUrl };
